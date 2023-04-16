bfc294e9e8f0bf5d7de6535030b3cc5846ffcd21
Sai Prakash <63905195+SaiSpr@users.noreply.github.com>
2023-04-16 21:13:23 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -1,23 +1,20 @@
-#-----------------------#
-# IMPORTING DES LIBRAIRIES #
-#-----------------------#
 
+# IMPORTing NECESSARY LIBRAIRIES 
 import streamlit as st
 import joblib
 import streamlit.components.v1 as components
-# import plotly.graph_objects as go
 import matplotlib.pyplot as plt
-# import plotly.express as px
 st.set_option('deprecation.showPyplotGlobalUse', False)
 import shap
 import requests as re
-# import numpy as np
+import numpy as np
 import pickle
 import json
 import pandas as pd
 from streamlit_echarts import st_echarts
 import seaborn as sns 
 plt.style.use('ggplot')
+
 
 
 st.title("Bank Loan Detection Web App")
@@ -27,7 +24,6 @@ st.image("image.jpg")
 
 
 st.sidebar.header('Select the Client_id:')
-
 
   
 st.title("****Calculating the probability that a customer will repay their credit or not****")  
@@ -42,8 +38,6 @@ cols_shap_local = pickle.load(list_file)
 print(cols_shap_local)
 
 
-
-#df_test_prod = pd.read_csv('df_test_ok_prod_100.csv', index_col=[0])
 df_test_prod = pd.read_csv('df_test_ok_prod_100_V7.csv', index_col=[0])
 df_test_prod['LOAN_DURATION'] = 1/df_test_prod['PAYMENT_RATE']
 df_test_prod.drop(columns=['TARGET'], inplace=True)
@@ -59,10 +53,8 @@ list_client_prod = df_test_prod['SK_ID_CURR'].tolist()
 client_id = st.sidebar.selectbox("Client Id list",list_client_prod)
 client_id = int(client_id)
 
-st.header(f'Credit request result for client {client_id}')
-
     
-id = client_id  
+# step = client_id
     
 
     
@@ -114,32 +106,19 @@ df_train_rembourse = df_train_rembourse[cols_dashbord]
   
 if st.button("Detection Result"):
     values = {
-    "client_id": id,
+    "client_id": client_id,
     }
 
 
     st.write(f"""### These are the details:\n
 
-    Client Id is: {id}\n
+    Client Id is: {client_id}\n """)
 
-                """)
-
-    res = re.post( url ="https://creditcard2-production.up.railway.app/predict", data = json.dumps(values))
+    res = re.post( url ="https://loan-production.up.railway.app/predict", data = json.dumps(values))
 
     json_str = json.dumps(res.json())
     
-        
-#     st.write(json_str)
-#     st.write(type(json_str))
     resp = json.loads(json_str)
-    
-#     prediction = res
-
-#     st.write(res)
-#     st.write(type(res))
-    
-#     st.write(resp)
-#     st.write(type(resp))
     
     pred = resp["prediction"]
 
@@ -149,134 +128,112 @@ if st.button("Detection Result"):
 
     st.header(f'*Result of the credit application for the customer {client_id} is:*')
 
-    st.write(pred)
-    st.write(type(pred))
     if pred == 1:
-      st.error('Loan Refused')
-      option_1 = {
-            "tooltip": {"formatter": "{a} <br/>{b} : {c}%"},
+        st.error('Loan Refused')
+        option_1 = {
+              "tooltip": {"formatter": "{a} <br/>{b} : {c}%"},
+              "graphic": {
+                  "type": "text",
+                  "left": "center",
+                  "top": "45%",
+                  "style": {
+                      "fill": "#000",
+                      "text": f"Probability: {probability_value_0:.2f}%",
+                      "font": "bold 22px 'Helvetica Neue', Helvetica, Arial, sans-serif"
+                  }
+              },
             "series": [
                 {
-                    "name": "Pressure",
-                    "type": "gauge",
-                    "axisLine": {
-                        "lineStyle": {
-                            "width": 10,
-                        },
+                    "type": "liquidFill",
+                    "data": [probability_value_1/100],
+                    "label": {
+                        "show": "true",
+                        "formatter": f'Probability: {probability_value_1:.2f}%',
+                        "color": "white",
+                        "insideColor": "white",
+                        "fontSize": 30
                     },
-                    "progress": {"show": "true", "width": 10},
-                    "detail": {"valueAnimation": "true", "formatter": "{value}"},
-                    "data": [{"value": probability_value_1, "name": "Probabilité %"}],
+                    "backgroundStyle": {"borderColor": "#156ACF", "borderWidth": 3, "color": "#ff7f50"},
+                    "itemStyle": {"opacity": 0.95, "shadowBlur": 10, "shadowColor": "rgba(0, 0, 0, 0.4)"},
                 }
             ],
         }
 
-      st_echarts(options=option_1, width="100%", key=0)
-      st.header(f'*The data that most influenced the calculation of the prediction for the client {client_id} is:*')
+        st_echarts(options=option_1, width="100%", height="600px", key=0)
 
-      explain_plot(client_id, pred)
+        st.header(f'*The data that most influenced the calculation of the prediction for the client {client_id} is:*')
+
+        # simulate explanation plot
+        num_features = 10
+        feature_names = [f'Feature {i}' for i in range(num_features)]
+        feature_importance = np.random.uniform(low=0, high=1, size=num_features)
+        explanation_df = pd.DataFrame({'Feature': feature_names, 'Importance': feature_importance})
+        st.bar_chart(explanation_df.set_index('Feature'))
+
     else:
         st.success('Loan Approved')
         option = {
-            "tooltip": {"formatter": "{a} <br/>{b} : {c}%"},
+              "tooltip": {"formatter": "{a} <br/>{b} : {c}%"},
+              "graphic": {
+                  "type": "text",
+                  "left": "center",
+                  "top": "45%",
+                  "style": {
+                      "fill": "#000",
+                      "text": f"Probability: {probability_value_0:.2f}%",
+                      "font": "bold 22px 'Helvetica Neue', Helvetica, Arial, sans-serif"
+                  }
+              },
             "series": [
                 {
-                    "name": "Pressure",
-                    "type": "gauge",
-                    "axisLine": {
-                        "lineStyle": {
-                            "width": 10,
-                        },
+                    "type": "liquidFill",
+                    "data": [probability_value_0/100],
+                    "label": {
+                        "show": "true",
+                        "formatter": f'Probability: {probability_value_0:.2f}%',
+                        "color": "white",
+                        "insideColor": "white",
+                        "fontSize": 30
                     },
-                    "progress": {"show": "true", "width": 10},
-                    "detail": {"valueAnimation": "true", "formatter": "{value}"},
-                    "data": [{"value": probability_value_0, "name": "Probabilité %"}],
+                    "backgroundStyle": {"borderColor": "#156ACF", "borderWidth": 3, "color": "#121847"},
+                    "itemStyle": {"opacity": 0.95, "shadowBlur": 10, "shadowColor": "rgba(0, 0, 0, 0.4)"},
                 }
             ],
         }
 
-        st_echarts(options=option, width="100%", key=0)
+        st_echarts(options=option, width="100%", height="600px", key=0)
 
-        st.header(f'*The data that most influenced the calculation of the prediction for the client {client_id} is:*')
+
+
+        st.header(f'*The highest predictive power and most important factors in descending order of {client_id} is:*')
         explain_plot(client_id, pred)
 
 
 
     st.header("*The most significant variables in descending order and which have a high predictive power.*")
 
-    st.image("Shap_features_global.png", use_column_width=True)
-
-    df_informations_client = df_test_prod[['SK_ID_CURR','CODE_GENDER','AGE', 'FLAG_OWN_CAR','FLAG_OWN_REALTY', 'CNT_CHILDREN',
-    'AGE','AMT_CREDIT', 'AMT_GOODS_PRICE', 'AMT_INCOME_TOTAL', 'EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3','LOAN_DURATION']]
+    st.image("shap_values_impact.png", use_column_width=True)
 
 
 
 
-    st.header(f"*Customer's personal information {client_id} :*")
+#     st.header(f"*Customer's personal information {client_id} :*")
     df_test_visu = df_test_prod[['SK_ID_CURR','AMT_CREDIT', 'AMT_GOODS_PRICE', 'AMT_INCOME_TOTAL', 'EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3', 'AGE', 'LOAN_DURATION']]
-
-    st.write(df_informations_client[df_informations_client['SK_ID_CURR']==client_id].transpose())
 
 
     #plot 
-    st.header(f'*Descriptive information about the customer {client_id}*'
+    st.header(f'*Information about the client: {client_id}*'
     )
     list_cols_dashboard = ['AMT_GOODS_PRICE', 'EXT_SOURCE_1', 'EXT_SOURCE_2', 'EXT_SOURCE_3', 'AGE', 'LOAN_DURATION',]
     for col in list_cols_dashboard:
-
-
-        #fig = ff.create_distplot(col,df_train, bin_size=[.1, .25, .5])
-        #st.plotly_chart(fig, use_container_width=True)
-        #st.pyplot()
-
-
-
          plt.figure(figsize=(12,8))
-         plt.gca().set_title(f'{col} At', size=30)
+         plt.gca().set_title(f'{col}', size=30)
          sns.distplot(df_train_rembourse[[col]], label='Rembourse', color='green', hist=False, bins=8)
          sns.distplot(df_train_not_rembourse[[col]], label='Non_rembourse', color='red', hist=False, bins=8)
          plt.axvline(x=float(df_test_visu[df_test_visu['SK_ID_CURR']== client_id][[col]].values),
                      color='blue', ls=':', lw=4, label=client_id)
-      #plt.annotate('Le client', xy = (000000.175, 100001), xytext = (1.75e-6, 100001),
-                     # arrowprops = {'facecolor': 'red', 'shrink': 0.1})
+
          plt.legend()
-      # Plot!
-      #st.plotly_chart(fig, use_container_width=True)
+
          st.pyplot()
             
-    option_3 = {
-            #"title": {"text": "Comparaison du client avec la base de données"},
-            "legend": {"data": ["（Payment default）", "（Non default payment）", f"the customer {client_id}"]},
-            "radar": {
-                "indicator": [
-                    {"name": "（Age）", "max": 70},
-                    {"name": "（AMT_GOODS_PRICE）", "max": 1800000.00},
-                    {"name": "（AMT_CREDIT）", "max": 2102490.00},
-                    {"name": "（AMT_INCOME_TOTAL）", "max": 540000.00},
-                    {"name": "（AMT_ANNUITY）", "max": 74416.50},
-                    {"name": "（LOAN_DURATION）", "max": 32.21},
-                ]
-            },
-            "series": [
-                {
-                    "name": "（Client_id vs. Database）",
-                    "type": "radar",
-                    "data": [
-                        {
-                            "value": [43.71 ,488972.41, 557778.53, 165611.76,  26481.74,20.76 ],
-                            "name": "（Payment default）",
-                        },
-                        {
-                            "value": [40.28 ,542738.51, 602651.16, 169077.47, 27163.73, 21.68 ],
-                            "name": "（Non default payment）",
-                        },
-                        {
-                            "value": list(df_test_prod[df_test_prod['SK_ID_CURR']==client_id][["AGE","AMT_GOODS_PRICE","AMT_CREDIT","AMT_INCOME_TOTAL","AMT_ANNUITY","LOAN_DURATION"]].squeeze())
-    ,
-                            "name": f"le client {client_id}",
-                        },
-                    ],
-                }
-            ],
-        }
-    st_echarts(option_3, height="600px")
